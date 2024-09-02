8675f834ba18d895fe752ecb4f0606e68a45173d
William McPherson <willmcpherson2@gmail.com>
2024-09-02 19:03:51 +1000

    Split tests up

--- tests/examples.rs
+++ tests/examples.rs
@@ -20,16 +20,44 @@ macro_rules! run {
 }
 
 #[test]
-fn test_examples() {
+fn test_bool() {
     run!("bool.soop", ":true");
+}
+
+#[test]
+fn test_function() {
     run!("function.soop", ":d");
+}
+
+#[test]
+fn test_inheritance() {
     run!(
         "inheritance.soop",
         "((:alice, :says, :hello), :to, :robert), :and, :alice, :says, :YEAH"
     );
+}
+
+#[test]
+fn test_list() {
     run!("list.soop", ":false, :true, :nil");
+}
+
+#[test]
+fn test_map() {
     run!("map.soop", ":alex, :bob, :charlie");
+}
+
+#[test]
+fn test_number() {
     run!("number.soop", ":s, :s, :s, :0");
+}
+
+#[test]
+fn test_option() {
     run!("option.soop", ":some, :yes");
+}
+
+#[test]
+fn test_state() {
     run!("state.soop", ":c");
 }

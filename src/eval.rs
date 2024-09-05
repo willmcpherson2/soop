95820c3b886f12273a7a2c6fe0cf179f99b4d205
William McPherson <willmcpherson2@gmail.com>
2024-09-05 19:38:14 +1000

    Fix and clean up env in pattern matching

--- src/eval.rs
+++ src/eval.rs
@@ -31,17 +31,17 @@ fn eval_lazy(env: Env, exp: Exp) -> Data {
     }
 }
 
-fn apply(mut env: Env, fun: Exp, arg: Exp) -> Data {
+fn apply(env: Env, fun: Exp, arg: Exp) -> Data {
     match eval_lazy(env.clone(), fun) {
         Data::Cons(cons_env, l, r) => {
-            env.extend(cons_env);
+            let env = extend(env, cons_env);
             match apply(env.clone(), *l, arg.clone()) {
                 Data::Error(_) => apply(env, *r, arg),
                 data => data,
             }
         }
         Data::Fun(fun_env, pat, body) => {
-            env.extend(fun_env);
+            let env = extend(env, fun_env);
             match pattern_match(env, *pat, arg) {
                 Ok(env) => eval_lazy(env, *body),
                 Err(e) => Data::Error(e),
@@ -52,14 +52,13 @@ fn apply(mut env: Env, fun: Exp, arg: Exp) -> Data {
     }
 }
 
-fn pattern_match(mut env: Env, pat: Exp, arg: Exp) -> Result<Env, Error> {
+fn pattern_match(env: Env, pat: Exp, arg: Exp) -> Result<Env, Error> {
     match pat {
         Let(var, exp, body) => match arg {
-            Let(_, arg_exp, arg_body) => {
-                env.extend(pattern_match(env.clone(), *exp, *arg_exp)?);
-                env.extend(pattern_match(env.clone(), *body, *arg_body)?);
-                Ok(env)
-            }
+            Let(_, arg_exp, arg_body) => Ok(extend(
+                pattern_match(env.clone(), *exp, *arg_exp)?,
+                pattern_match(env, *body, *arg_body)?,
+            )),
             other => Err(Error::PatternMatchExp(
                 Box::new(Let(var, exp, body)),
                 Box::new(other),
@@ -67,10 +66,11 @@ fn pattern_match(mut env: Env, pat: Exp, arg: Exp) -> Result<Env, Error> {
         },
         Cons(l, r) => match eval_lazy(env.clone(), arg) {
             Data::Cons(arg_env, arg_l, arg_r) => {
-                env.extend(arg_env);
-                env.extend(pattern_match(env.clone(), *l, *arg_l)?);
-                env.extend(pattern_match(env.clone(), *r, *arg_r)?);
-                Ok(env)
+                let env = extend(env, arg_env);
+                Ok(extend(
+                    pattern_match(env.clone(), *l, *arg_l)?,
+                    pattern_match(env, *r, *arg_r)?,
+                ))
             }
             other => Err(Error::PatternMatchData(
                 Box::new(Cons(l, r)),
@@ -79,10 +79,11 @@ fn pattern_match(mut env: Env, pat: Exp, arg: Exp) -> Result<Env, Error> {
         },
         Fun(pat, body) => match eval_lazy(env.clone(), arg) {
             Data::Fun(arg_env, arg_pat, arg_body) => {
-                env.extend(arg_env);
-                env.extend(pattern_match(env.clone(), *pat, *arg_pat)?);
-                env.extend(pattern_match(env.clone(), *body, *arg_body)?);
-                Ok(env)
+                let env = extend(env, arg_env);
+                Ok(extend(
+                    pattern_match(env.clone(), *pat, *arg_pat)?,
+                    pattern_match(env, *body, *arg_body)?,
+                ))
             }
             other => Err(Error::PatternMatchData(
                 Box::new(Fun(pat, body)),
@@ -90,11 +91,10 @@ fn pattern_match(mut env: Env, pat: Exp, arg: Exp) -> Result<Env, Error> {
             )),
         },
         App(l, r) => match arg {
-            App(arg_l, arg_r) => {
-                env.extend(pattern_match(env.clone(), *l, *arg_l)?);
-                env.extend(pattern_match(env.clone(), *r, *arg_r)?);
-                Ok(env)
-            }
+            App(arg_l, arg_r) => Ok(extend(
+                pattern_match(env.clone(), *l, *arg_l)?,
+                pattern_match(env, *r, *arg_r)?,
+            )),
             other => Err(Error::PatternMatchExp(Box::new(App(l, r)), Box::new(other))),
         },
         Var(var) => Ok(bind(env, var, arg)),
@@ -122,6 +122,11 @@ fn resolve(mut env: Env, var: String) -> Data {
         Some(Thunk(env, exp)) => eval_lazy(env, exp),
         None => Data::Error(Error::Undefined(var)),
     }
+}
+
+fn extend(mut env: Env, new_env: Env) -> Env {
+    env.extend(new_env);
+    env
 }
 
 pub fn deep_to_exp(deep: Deep) -> Exp {
